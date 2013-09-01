f02afa9102e4984e90f7c6ba5f13144fe00125af
Jack Cameo <Jack@Klatchit.com>
2013-09-01 05:10:13 +0000

    Testing fs.readFileSync

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = fs.readFileSync('index.html');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(fs);
 });
 
 var port = process.env.PORT || 5000;
